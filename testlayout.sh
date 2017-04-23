d4df4e90b6c43d17615a5d9715376f31828281e8
binaryplease <binaryplease@gmail.com>
2017-04-23 18:19:59 +0200

    working, only absolute paths

---
+++ testlayout.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+i3-msg "workspace 6; append_layout ./testlayout.json"
+
+exec termite --hold \
+				--exec="htop" \
+				--name="window1"
+				--class="window1" \
+				--title="window1" &
+
+exec termite --hold \
+				--exec="htop" \
+				--name="window2"
+				--class="window2" \
+				--title="window2" &
+
+exec termite --hold \
+				--exec="htop" \
+				--name="window3"
+				--class="window3" \
+				--title="window3" &
